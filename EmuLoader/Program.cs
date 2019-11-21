676874a7a881bd0ba0fe5b5e963b852ad4cfc5eb
Fernando <fernandolorenzon@gmail.com>
2019-11-20 19:18:29 -0800

    -added block multiple instances

--- EmuLoader/Program.cs
+++ EmuLoader/Program.cs
@@ -2,20 +2,32 @@
 using System.Collections.Generic;
 using System.Windows.Forms;
 using EmuLoader.Forms;
+using System.Threading;
 
 namespace EmuLoader
 {
     static class Program
     {
+        private static string appGuid = "28f3f92f-34a5-445e-a88c-13a9e13e03fd";
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            using (Mutex mutex = new Mutex(false, "Global\\" + appGuid))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("Instance already running");
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new FormMain());
+            }
         }
     }
 }
